741261a800bd2fd1e995f45db25467196e93d336
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-11-27 10:27:02 -0800

    couch

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,16 +7,16 @@
  */
 int check_cycle(listint_t *list)
 {
-    listint_t *slow = list, *fast = list;
+	listint_t *slow = list, *fast = list;
 
-    while (slow && fast && fast->next) {
-        slow = slow->next;
-        fast = fast->next->next;
+	while (slow && fast && fast->next) {
+		slow = slow->next;
+		fast = fast->next->next;
 
-        if (slow == fast) {
-            return (1);
-        }
-    }
+		if (slow == fast) {
+			return (1);
+		}
+	}
 
-    return (0);
+	return (0);
 }
