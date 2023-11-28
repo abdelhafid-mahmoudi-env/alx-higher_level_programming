4fcc8d1927ffb52ef00ea41d8371a0461dd0233c
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-11-28 10:50:58 -0800

    test-

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for num1 in range(10):
-    for num2 in range(10):
-        if num1 == 9 and num2 == 9:
-            print("{}{}".format(num1, num2), end="\n")
-        else:
-            print("{}{}".format(num1, num2), end=", ")
+for i in range(100):
+    if i != 99:
+        print("{:02d}, ".format(i), end="")
+    else:
+        print("{:02d}".format(i))
