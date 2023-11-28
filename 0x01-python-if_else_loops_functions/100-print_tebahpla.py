e294f434170c0b1a103020f54757ee298f9cc7f3
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-11-28 11:06:45 -0800

    test-

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for char in range(ord('z'), ord('A') - 1, -1):
-    print("{:c}".format(char if char % 2 == 0 else char - 32), end="")
+    print("{:c}".format(char if char % 2 == 0 else char + 32), end="")
