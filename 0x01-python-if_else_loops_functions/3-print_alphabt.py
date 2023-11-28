2b2b1bca9064f3cf791f1ec4b043a0428d405f5e
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-11-28 10:38:47 -0800

    test-

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if letter == 101 or letter == 113:
+for letter_code in range(97, 123):
+    if letter_code == 101 or letter_code == 113:
         continue
-    print_char = chr(letter)
-    print(print_char, end="")
+    else:
+        letter = chr(letter_code)
+        print(letter, end="")
