b6c2405cfcb9a17d0a860bf865739985efad1be7
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-11-28 10:57:26 -0800

    test-

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(s):
+    for char in s:
+        if ord('a') <= ord(char) <= ord('z'):
+            uppercase_char = chr(ord(char) - 32)
+        else:
+            uppercase_char = char
+        print("{}".format(uppercase_char), end="")
+    print()
