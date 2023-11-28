9a15eafa4f15b16286120b3a340501f1d81e6e84
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-11-28 10:26:04 -0800

    test1

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-
-# Constructing the alphabet string using list comprehension
-alphabet = ''.join([chr(i) for i in range(97, 123)])
-
-# Printing the alphabet string
-print(alphabet, end="")
+for i in range(97, 123):
+    print("{}".format(chr(i)), end="")
