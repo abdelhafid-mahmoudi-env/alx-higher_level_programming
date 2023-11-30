9134686adfaf435f490385d208a821fa0f37d636
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-11-30 05:38:07 -0800

    test

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-from add_0 import add
+def main():
+    from add_0 import add
 
-a = 1
-b = 2
-print(f"{a} + {b} = {add(a, b)}")
+    a = 1
+    b = 2
+    result = add(a, b)
+    print(f"{a} + {b} = {result}")
+
+if __name__ == "__main__":
+    main()
