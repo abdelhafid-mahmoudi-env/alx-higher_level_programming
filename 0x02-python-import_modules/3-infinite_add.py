db7206b6dd7f5df0b1e854d3def25d5317340934
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-11-30 10:23:37 -0800

    calculation

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ import sys
 
 if __name__ == "__main__":
     total = 0
-    for arg in sys.argv[1:]:  # Skip the script name, start from the first argument
-        total += int(arg)  # Convert argument to integer and add to total
+    for arg in sys.argv[1:]:
+        total += int(arg)
     print(total)
