361ab6fb40639b2b889a9d13f6d419a8aa4313c3
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-12-02 02:57:39 -0800

    2 replace in list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    for num in reversed(my_list):
-        print("{}".format(num))
+    if not my_list:
+        return  # If the list is empty, do nothing
+
+    index = len(my_list) - 1  # Start from the last index
+    while index >= 0:
+        print("{}".format(my_list[index]))
+        index -= 1
