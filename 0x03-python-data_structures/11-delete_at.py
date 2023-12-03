14c96cf13d931da438aa649ed843c91b449a2c04
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-12-03 00:37:05 -0800

    delete at test 1

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-def delete_at(my_list=[], idx=0):
-    if 0 <= idx < len(my_list):
-        my_list = my_list[:idx] + my_list[idx+1:]
+def reverse_list(my_list=[]):
+    start = 0
+    end = len(my_list) - 1
+    while start < end:
+        my_list[start], my_list[end] = my_list[end], my_list[start]
+        start += 1
+        end -= 1
     return my_list
