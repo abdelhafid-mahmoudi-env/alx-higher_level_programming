8a95534bcc115e5faea3c8e9dda48aa57a1d65b7
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-12-06 12:05:38 -0800

    python

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,48 +1,58 @@
 #include <Python.h>
+#include <object.h>
+#include <listobject.h>
+#include <bytesobject.h>
 
 void print_python_bytes(PyObject *p)
 {
-    long int size;
-    int i;
-    char *trying_str = NULL;
+    PyBytesObject *bytesObj;
+    char *str;
+    Py_ssize_t size, i;
 
     printf("[.] bytes object info\n");
+
     if (!PyBytes_Check(p))
     {
         printf("  [ERROR] Invalid Bytes Object\n");
         return;
     }
 
-    PyBytes_AsStringAndSize(p, &trying_str, &size);
+    bytesObj = (PyBytesObject *)p;
+    PyBytes_AsStringAndSize(p, &str, &size);
 
     printf("  size: %li\n", size);
-    printf("  trying string: %s\n", trying_str);
-    if (size < 10)
-        printf("  first %li bytes:", size + 1);
-    else
-        printf("  first 10 bytes:");
-    for (i = 0; i <= size && i < 10; i++)
-        printf(" %02hhx", trying_str[i]);
+    printf("  trying string: %s\n", str);
+    printf("  first %li bytes:", (size < 10) ? size + 1 : 10);
+
+    for (i = 0; i < size && i < 10; i++)
+        printf(" %02hhx", str[i]);
+
     printf("\n");
 }
 
 void print_python_list(PyObject *p)
 {
-    long int size = PyList_Size(p);
-    int i;
-    PyListObject *list = (PyListObject *)p;
-    const char *type;
+    PyListObject *listObj;
+    Py_ssize_t size, i;
+    PyObject *element;
 
     printf("[*] Python list info\n");
+
+    size = PyList_Size(p);
+    listObj = (PyListObject *)p;
+
     printf("[*] Size of the Python List = %li\n", size);
-    printf("[*] Allocated = %li\n", list->allocated);
+    printf("[*] Allocated = %li\n", listObj->allocated);
+
     for (i = 0; i < size; i++)
     {
-        PyObject *item = list->ob_item[i];
-        type = item->ob_type->tp_name;
-        printf("Element %i: %s\n", i, type);
-        
+        element = listObj->ob_item[i];
+        const char *type = Py_TYPE(element)->tp_name;
+
+        printf("Element %li: %s\n", i, type);
+
         if (strcmp(type, "bytes") == 0)
-            print_python_bytes(item);
+            print_python_bytes(element);
     }
 }
+
