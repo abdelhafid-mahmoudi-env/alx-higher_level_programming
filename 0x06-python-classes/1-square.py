836849fc5549517535febe959e2922ad641b71d3
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-12-19 12:42:02 -0800

    test

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square with a private instance attribute 'size'."""
+    
+    def __init__(self, size):
+        """Initializes a new instance of the Square class."""
+        self.__size = size
