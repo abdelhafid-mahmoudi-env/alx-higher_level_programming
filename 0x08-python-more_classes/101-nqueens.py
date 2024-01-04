085b561e4b41781e911ebb204d159ea19c721255
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-04 15:55:58 -0800

    test

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -30,10 +30,6 @@ def solve_nqueens(n):
                 solve(board, row + 1)
                 board[row] = -1
 
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
     board = [-1] * n
     solve(board, 0)
 
@@ -46,6 +42,10 @@ if __name__ == "__main__":
         N = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
         sys.exit(1)
 
     solve_nqueens(N)
