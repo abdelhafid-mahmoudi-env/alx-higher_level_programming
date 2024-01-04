85215345db5c1e28f98a8fe9e75d83414a9a963b
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-04 08:24:06 -0800

    rec

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""
+This module provides a simple Rectangle class.
+"""
 
-# Define an empty class named Rectangle
 class Rectangle:
     # The 'pass' statement is used to indicate that the class is empty
     pass
