6559dd7b811edd5ed840c9d8fec9afc05e2e64e2
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-04 16:31:45 -0800

    test

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -54,12 +54,9 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
     try:
         N = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-
     solve_nqueens(N)
-
