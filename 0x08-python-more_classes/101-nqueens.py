a33e6be8a5e121838af769135b70c8de06ca7dfb
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-04 16:16:15 -0800

    test

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -11,41 +11,41 @@ def is_safe(board, row, col):
             return False
     return True
 
+
 def print_solution(board):
     """Print the N Queens board."""
-    solution = [[i, board[i]] for i in range(len(board))]
+    solution = [[i, board[i]] for i in range(n)]
     print(solution)
+
 
 def solve(board, row):
     """Recursively solve the N Queens puzzle."""
-    if row == len(board):
+    if row == n:
         print_solution(board)
         return
 
-    for col in range(len(board)):
+    for col in range(n):
         if is_safe(board, row, col):
             board[row] = col
             solve(board, row + 1)
             board[row] = -1
 
+
 def solve_nqueens(n):
     board = [-1] * n
     solve(board, 0)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
     try:
         N = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
-
     solve_nqueens(N)
-
