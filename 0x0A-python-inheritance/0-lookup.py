2462e22e45f06de62afd0cab15c5bd887a8b22e2
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-08 15:08:49 -0800

    inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,9 +6,5 @@ and methods of an object
 """
 
 def lookup(obj):
-    """
-    Returns a list of available attributes and methods of an object
-    :param obj: The object to look up
-    :return: list of attributes and methods
-    """
+    """Returns a list of available attributes and methods of an object"""
     return dir(obj)
