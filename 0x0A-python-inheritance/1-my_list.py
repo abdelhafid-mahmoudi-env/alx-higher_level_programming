ff8f76b270f1d0673dfc98f79b73e0e27c69f79a
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-08 15:16:53 -0800

    inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module 1-my_list
+Contains class MyList that inherits from list
+Includes method print_sorted to print the list in ascending order
+"""
+
+class MyList(list):
+    """Inherits from list"""
+
+    def print_sorted(self):
+        """Prints the list in ascending order"""
+        
+        print(sorted(self))
