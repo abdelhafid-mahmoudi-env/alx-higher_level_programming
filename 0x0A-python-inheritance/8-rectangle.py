240f3a6d42e79161bf0e0e6283c7db27a2f46417
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-08 16:15:24 -0800

    my list

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
-""" Module for the Rectangle class that inherits from BaseGeometry. """
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+""" Module for the Rectangle class that inherits from BaseGeometry. """
 
 
 class Rectangle(BaseGeometry):
-    """ Rectangle class that inherits from BaseGeometry. """
+    """Rectangle class that inherits from BaseGeometry"""
 
     def __init__(self, width, height):
-        """
-        Initializes a new Rectangle.
-
-        Args:
-            width (int): The width of the rectangle, must be a positive integer.
-            height (int): The height of the rectangle, must be a positive integer.
-        """
-        # Validate that width and height are positive integers using integer_validator
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        
-        # Set the private attributes __width and __height
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
