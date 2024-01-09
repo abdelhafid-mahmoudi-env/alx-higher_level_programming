0f93d6c3066e9817b118a26c74de8688d15f8159
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-08 16:00:34 -0800

    my list

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
-"""
-Module 6-base_geometry
-Defines a class BaseGeometry with a public instance method.
-"""
+""" Module 6-base_geometry Defines a class BaseGeometry."""
 
 
 class BaseGeometry:
     """A class BaseGeometry."""
 
     def area(self):
-        """
-        Raises an Exception with a message indicating that the method is not implemented.
-        This method is expected to be overridden in derived classes.
-
-        Raises:
-            Exception: If the method is not implemented in a derived class.
-        """
+        """ Raises an Exception with a message indicating that the method is not implemented. """
         raise Exception("area() is not implemented")
