be9a51054cc1764c0bdd5986118fe76df15c8817
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-10 09:36:08 +0000

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-""" Ajoute les arguments à une liste, puis enregistre dans un fichier """
+""" Appends arguments to a list and then saves it to a file. """
 
 import sys
 
 if __name__ == "__main__":
-    save_json = __import__('5-save_to_json_file').save_to_json_file
-    load_json = __import__('6-load_from_json_file').load_from_json_file
+    # Import necessary functions
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
     try:
-        items = load_json("add_item.json")
+        # Load the existing list
+        my_list = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        items = []
+        # Initialize an empty list
+        my_list = []
 
-    items.extend(sys.argv[1:])
-    save_json(items, "add_item.json")
+    # Append the arguments to the list
+    my_list.extend(sys.argv[1:])
+
+    # Save the list to a file
+    save_to_json_file(my_list, "add_item.json")
