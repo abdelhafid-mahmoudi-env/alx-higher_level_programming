e7f2b641a2c2d6fe0b8aeb314ce6dd9a1f02cf91
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-10 08:54:37 +0000

    test

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
-"""method for student creation"""
+"""Module définissant la classe Student."""
 
 
 class Student:
-    """Student obj"""
+    """Classe représentant un étudiant."""
 
     def __init__(self, first_name, last_name, age):
+        """Initialise une nouvelle instance de la classe Student.
+
+        Args:
+            first_name (str): Le prénom de l'étudiant.
+            last_name (str): Le nom de famille de l'étudiant.
+            age (int): L'âge de l'étudiant.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """Renvoie une représentation dictionnaire de l'instance Student.
+
+        Returns:
+            dict: Un dictionnaire contenant attributs de l'étudiant.
+        """
         return self.__dict__
