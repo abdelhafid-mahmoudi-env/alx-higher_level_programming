be2778d9b8020e283a28c91037b064457981adc7
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-10 09:44:16 +0000

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
 """Module for managing JSON data."""
 
-import json
-import os.path
 import sys
+import os.path
 from sys import argv
+from importlib import import_module
 
-# Rename the functions as needed
-save_json_data = __import__('7-save_to_json_file').save_to_json_file
-load_json_data = __import__('8-load_from_json_file').load_from_json_file
+# Check if the save_to_json_file and load_from_json_file functions exist
+try:
+    save_to_json_file = import_module('7-save_to_json_file').save_to_json_file
+    load_from_json_file = import_module('8-load_from_json_file').load_from_json_file
+except (ImportError, AttributeError):
+    sys.exit(1)
 
 json_filename = "add_item.json"
 json_data = []
 
+# Load existing data from the JSON file if it exists
 if os.path.exists(json_filename):
-    json_data = load_json_data(json_filename)
+    json_data = load_from_json_file(json_filename)
 
+# Add command line arguments to the JSON data
 for item in argv[1:]:
     json_data.append(item)
 
-save_json_data(json_data, json_filename)
+# Save the updated JSON data to the file
+save_to_json_file(json_data, json_filename)
