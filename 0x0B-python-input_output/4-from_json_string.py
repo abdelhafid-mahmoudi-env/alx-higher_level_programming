62a4ceca70d12c883a54db5c64dc3b7075f927ed
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-10 08:04:27 +0000

    test

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A module for converting a JSON string into a Python data structure.
+"""
+
+import json
+
+def from_json_string(my_str):
+    """
+    Returns a Python data structure represented by a JSON string.
+
+    Args:
+        my_str (str): The JSON string to be converted.
+
+    Returns:
+        The Python data structure represented by the JSON string.
+    """
+    return json.loads(my_str)
