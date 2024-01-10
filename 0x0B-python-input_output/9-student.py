5790a13dbbece03248759fd583751f76e258636f
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-10 08:50:40 +0000

    test

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,6 +4,7 @@ class Student:
     """Class representing a student with first_name, last_name, and age."""
 
     def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance with first_name, last_name, and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
