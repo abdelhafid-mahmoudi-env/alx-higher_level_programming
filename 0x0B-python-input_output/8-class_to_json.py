52a503456ffa132a3ba12b2b7df24c0d7176e797
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-10 08:39:45 +0000

    test

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
-
 def class_to_json(obj):
-    return vars(obj)
+    """Converts an object to a dictionary for JSON serialization."""
+    if hasattr(obj, '__dict__'):
+        return obj.__dict__
+    elif hasattr(obj, '__slots__'):
+        return {key: getattr(obj, key) for key in obj.__slots__ if hasattr(obj, key)}
+    else:
+        return {}
