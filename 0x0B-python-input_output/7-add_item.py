041dd13354a8780bef9d486737e7d6fbba76970c
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-10 08:31:41 +0000

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,24 +1,20 @@
-#!/usr/bin/python3
-"""A script that adds all command-line arguments to a Python list, and then saves them to a file."""
-
+"""Module for saving to json"""
+import json
+import os.path
 import sys
+from sys import argv
 
-# Importing the necessary functions
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-def main():
+if __name__ == "__main__":
     filename = "add_item.json"
+    json_list = []
 
-    try:
-        # Loading the existing list from the file
-        items = load_from_json_file(filename)
-    except FileNotFoundError:
-        # If the file does not exist, start with an empty list
-        items = []
+    if os.path.exists(filename):
+        json_list = load_from_json_file(filename)
 
-    # Add command-line arguments to the list
-    items.extend(sys.argv[1:])
+    for index in argv[1:]:
+        json_list.append(index)
 
-    # Save the updated list back to the file
-    save_to_json_file(items, filename)
+    save_to_json_file(json_list, filename)
