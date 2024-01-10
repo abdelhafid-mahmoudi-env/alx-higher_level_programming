8fd84d85b8d2be351027b3fd518daaae7777fe20
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-10 07:53:40 +0000

    test

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""
-A module for reading and printing the contents of a text file.
-
-This module provides a function to read a given text file and print its contents to standard output.
-"""
+"""Module for reading and printing the content of a text file."""
 
 
 def read_file(filename=""):
