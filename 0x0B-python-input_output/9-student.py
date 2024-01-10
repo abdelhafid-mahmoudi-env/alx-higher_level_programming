12a7f0cab6b072f7353bd69d0e5c25a7e3ed805d
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-10 08:52:32 +0000

    test

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
+"""method for student creation"""
+
 
 class Student:
-    """Class representing a student with first_name, last_name, and age."""
+    """Student obj"""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a Student instance with first_name, last_name, and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Return a dictionary representation of a Student instance."""
-        return {
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'age': self.age
-        }
+        return self.__dict__
