15159c988322875971c5cb1a55b249064531541d
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-10 09:30:35 +0000

    test

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""Module for converting an object of a class to a dictionary for JSON."""
+
 
 def class_to_json(obj):
-    """Return a dictionary description of an object for JSON serialization."""
+    """Return the description of an object for JSON serialization.
+
+    Args:
+        obj: An instance of a class.
+
+    Returns:
+        dict: A dictionary describing the object.
+    """
     return obj.__dict__
