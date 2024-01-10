008ace7e1b8a3269db7d92f4cf02b0dfc34e736e
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-10 08:35:18 +0000

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,27 +1,10 @@
 #!/usr/bin/python3
-import sys
-import os.path
-import json
 
-def save_to_json_file(my_obj, filename):
-    """Save an object to a JSON file."""
-    with open(filename, mode="w", encoding="utf-8") as file:
-        json.dump(my_obj, file)
-
-def load_from_json_file(filename):
-    """Load an object from a JSON file."""
-    if not os.path.exists(filename):
-        return []
-    with open(filename, mode="r", encoding="utf-8") as file:
-        return json.load(file)
-
-
-if __name__ == "__main__":
-    filename = "add_item.json"
-    try:
-        my_list = load_from_json_file(filename)
-    except FileNotFoundError:
-        my_list = []
-
-    my_list.extend(sys.argv[1:])
-    save_to_json_file(my_list, filename)
+def class_to_json(obj):
+    """Converts an object to a dictionary for JSON serialization."""
+    if hasattr(obj, '__dict__'):
+        return obj.__dict__
+    elif hasattr(obj, '__slots__'):
+        return {key: getattr(obj, key) for key in obj.__slots__ if hasattr(obj, key)}
+    else:
+        return {}
