9dc63c3bbc160b2238b876b93e8988cd74201953
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-10 08:24:22 +0000

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Adds arguments to a list, then saves it to a file """
+""" Adds arguments to a list, then saves them in a file """
 
 import sys
 
@@ -10,13 +10,13 @@ if __name__ == "__main__":
 
     try:
         # Loading the existing list
-        current_list = load_from_json_file("add_item.json")
+        items = load_from_json_file("add_item.json")
     except FileNotFoundError:
         # Initializing an empty list
-        current_list = []
+        items = []
 
-    # Adding the arguments to the current list
-    current_list.extend(sys.argv[1:])
+    # Adding the arguments to the list
+    items.extend(sys.argv[1:])
 
-    # Saving the current list to a file
-    save_to_json_file(current_list, "add_item.json")
+    # Saving the list in a file
+    save_to_json_file(items, "add_item.json")
