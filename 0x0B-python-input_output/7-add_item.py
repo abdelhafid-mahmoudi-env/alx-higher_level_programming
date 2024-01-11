94abda390a5ebbbbfcb82dca440edffda88c4c75
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-11 08:52:39 +0000

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,31 +1,17 @@
-#!/usr/bin/python3
-"""Module for managing JSON data."""
+mport sys
+import os
+import json
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
-import sys
-import os.path
-from sys import argv
-from importlib import import_module
+# Check if the JSON file exists, if not, create an empty list
+if not os.path.isfile("add_item.json"):
+    initial_list = []
+else:
+    initial_list = load_from_json_file("add_item.json")
 
-# Check if the save_to_json_file and load_from_json_file functions exist
-try:
-    save_to_json_file = import_module('7-save_to_json_file').save_to_json_file
-    load_from_json_file = import_module('8-load_from_json_file').load_from_json_file
-except (ImportError, AttributeError):
-    sys.exit(1)
+# Extend the list with command-line arguments
+initial_list.extend(sys.argv[1:])
 
-json_filename = "add_item.json"
-json_data = []
-
-# Load existing data from the JSON file if it exists
-if os.path.exists(json_filename):
-    json_data = load_from_json_file(json_filename)
-
-# Add command line arguments to the JSON data
-for item in argv[1:]:
-    json_data.append(item)
-
-# Save the updated JSON data to the file
-save_to_json_file(json_data, json_filename)
-
-# Print the content of the JSON file
-print(json_data)
+# Save the updated list to the JSON file
+save_to_json_file(initial_list, "add_item.json")
