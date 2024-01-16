1b83ae0117a98fb08175e3c49768f1234ea04021
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-16 20:31:58 +0000

    test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,36 +24,54 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """Get or set the width of the rectangle."""
+        """Get the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        self.__validate_positive_integer(value, "width")
         self.__width = value
 
     @property
     def height(self):
-        """Get or set the height of the rectangle."""
+        """Get the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        self.__validate_positive_integer(value, "height")
         self.__height = value
 
     @property
     def x(self):
-        """Get or set the x coordinate."""
+        """Get the x coordinate of the rectangle."""
         return self.__x
 
     @x.setter
     def x(self, value):
+        self.__validate_non_negative_integer(value, "x")
         self.__x = value
 
     @property
     def y(self):
-        """Get or set the y coordinate."""
+        """Get the y coordinate of the rectangle."""
         return self.__y
 
     @y.setter
     def y(self, value):
+        self.__validate_non_negative_integer(value, "y")
         self.__y = value
+
+    def __validate_positive_integer(self, value, attribute):
+        """Validate that a value is a positive integer."""
+        if type(value) is not int:
+            raise TypeError(f"{attribute} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{attribute} must be > 0")
+
+    def __validate_non_negative_integer(self, value, attribute):
+        """Validate that a value is a non-negative integer."""
+        if type(value) is not int:
+            raise TypeError(f"{attribute} must be an integer")
+        if value < 0:
+            raise ValueError(f"{attribute} must be >= 0")
