92e4f24ad455247201fba32b9e6f710af9e4a377
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-16 21:15:59 +0000

    test

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,15 +7,19 @@ class Square(Rectangle):
     """Square class that inherits from Rectangle."""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize a new Square.
-
-        Args:
-            size (int): The size of the square.
-            x (int): The x coordinate.
-            y (int): The y coordinate.
-            id (int): The id of the object.
-        """
+        """Initialize a new Square."""
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """Return the string representation of the Square."""
