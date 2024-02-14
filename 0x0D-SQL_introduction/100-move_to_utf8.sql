60ca10444e93eaa20e9587161e0f303de69f0192
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-02-14 01:30:35 -0800

    test

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,10 @@
--- Convert the database character set and collation
+-- Change character SET and COLLATE for the database and table
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
 USE hbtn_0c_0;
+
+-- Convert the entire table to use utf8mb4 character set and collation
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Specifically change the character set and collation for the 'name' column
 ALTER TABLE first_table CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
