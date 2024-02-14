4ded0548717329832696d3b52394c45dc2d5ada2
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-02-14 01:52:14 -0800

    test

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,10 @@
 USE hbtn_0c_0;
 
--- Convert the table to UTF8
+-- Convert the database collation
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the table collation
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the field 'name' to UTF8
-ALTER TABLE first_table MODIFY COLUMN `name` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert the collation of the name field
+ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
