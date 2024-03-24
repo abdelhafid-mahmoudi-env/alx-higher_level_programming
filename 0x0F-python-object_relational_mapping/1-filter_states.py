6341d454bcd394232e61a610d79cb61d7e8452f8
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-03-24 08:17:44 -0700

    select states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,21 +16,18 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
 
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-        states = cursor.fetchall()
-        for state in states:
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    states = cursor.fetchall()
+    for state in states:
+        if state[1][0] == 'N':
             print(state)
-        cursor.close()
-        db.close()
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-        sys.exit(1)
+    cursor.close()
+    db.close()
