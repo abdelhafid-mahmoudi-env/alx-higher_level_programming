818f08b95354ecd7eac30f5bf50cf9900c9646ba
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-03-24 08:11:45 -0700

    select states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+Script that lists all states with a name starting
+with N (upper N) from the database hbtn_0e_0_usa
 """
 
 import sys
@@ -33,4 +34,3 @@ if __name__ == "__main__":
     except MySQLdb.Error as e:
         print("MySQL Error:", e)
         sys.exit(1)
-
