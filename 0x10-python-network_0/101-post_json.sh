92ce6361b8ffd0c2b58c82f97e3d8adab1a53192
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-03-27 14:10:28 -0700

    test

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Sends a JSON POST request to a URL with the contents of a file in the body
 if [ -f "$2" ]; then
-    curl -s -X POST "$1" -H "Content-Type: application/json" -d @"$2"
+    curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
     echo ""
 else
     echo "Not a valid JSON"
