0f9f4310d8dffb7fc54c0d88d765a6208b0f3dc7
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-03-27 13:46:31 -0700

    test

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes to send a POST request with specific data and display the response body
-curl "$1" -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl "$1" -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD"
