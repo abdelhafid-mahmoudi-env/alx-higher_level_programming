3216a40f9a2360ce81cdb6c4d3c77ce1912ccd26
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-03-27 14:27:47 -0700

    test

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me causing the server to respond with "You got me!"
-curl -sL -X PUT 0.0.0.0:5000/catch_me -d "user_id=98"
+curl -sL -X PUT -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me -d "user_id=98"
