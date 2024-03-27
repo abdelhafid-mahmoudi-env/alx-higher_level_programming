95354878d7c0ac1d88ad18ced539115ca0e04766
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-03-27 13:23:34 -0700

    test

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -2,7 +2,6 @@
 # This script sends a GET request to a specified URL and displays the body of the response.
 # It includes a header 'X-School-User-Id' with the value '98' in the request.
 # Usage: ./4-header.sh <URL>
-#   <URL>: The URL to which the GET request will be sent
+URL=$1
 
-# Send GET request with the specified header and display the response body
-curl -s -H "X-School-User-Id: 98" "$1"
+curl -s -H "X-School-User-Id: 98" $URL
