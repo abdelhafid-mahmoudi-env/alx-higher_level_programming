7de26ae7ae1784935f7912eb8129c65698790156
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-03-27 12:55:33 -0700

    test

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# Description: Sends a GET request to a URL and displays the body of the response if the status code is 200.
-# Usage: ./1-body.sh <URL>
-# Arguments:
-#   <URL>: The URL to send the GET request to.
-
-# Send a GET request to the provided URL, follow redirects, and suppress progress output
-response=$(curl -sX GET "$1" -L)
-
-# Display the body of the response
-echo "$response"
+# Sends a GET request to the URL and displays the body of the response
+curl -sL "$1"
