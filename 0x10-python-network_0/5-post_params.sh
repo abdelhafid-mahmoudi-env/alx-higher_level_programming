969328bfebef9d4071e7321ee7a2cfbc40bce8a6
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-03-27 13:37:35 -0700

    test

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Declare email and subject variables
-email="test@gmail.com"
-subject="I will always be here for PLD"
 
 # Sends a POST request with email and subject parameters to the provided URL
-curl "$1" -sX POST -d "email=$email&subject=$subject"
+curl "$1" -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD"
