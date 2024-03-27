c02ff498e4edbeaab6d63a3f57a4dcfa963530ae
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-03-27 13:42:58 -0700

    test

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-# Takes in a URL as an argument and sends a POST request with specific data
-# Usage: ./5-post_params.sh <URL>
-#   <URL>: The URL to which the POST request will be sent
-
-# Check if the script is provided with exactly one argument
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send a POST request with specific data and display the response body
+# Takes to send a POST request with specific data and display the response body
 curl "$1" -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
