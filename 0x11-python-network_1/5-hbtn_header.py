a39bcc26cfe488763e033a529820ca0d64a54804
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-03-28 13:00:37 -0700

    test

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This script takes in a URL, sends a request to the URL, and displays the value of
+This script takes in a URL, sends a request
+to the URL, and displays the value of
 the variable X-Request-Id in the response header.
 """
 
