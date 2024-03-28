4ac948a9c8610d55ee25f784e95ec3edf2c2d14e
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-03-28 13:09:52 -0700

    test

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -13,10 +13,12 @@ if __name__ == "__main__":
     else:
         q = sys.argv[1]
     try:
-        response = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+        response = requests.post('http://0.0.0.0:5000/search_user',
+                                 data={'q': q})
         json_response = response.json()
         if json_response:
-            print("[{}] {}".format(json_response.get('id'), json_response.get('name')))
+            print("[{}] {}".format(json_response.get('id'),
+                                   json_response.get('name')))
         else:
             print("No result")
     except ValueError:
