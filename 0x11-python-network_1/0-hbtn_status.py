d26e60ae5abb46855068c17545a04a62ec156178
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-03-28 08:33:00 -0700

    test

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A Python script that fetches https://alx-intranet.hbtn.io/status
+This script demonstrates the usage of the urllib package to make a request
+and print the body of the response in a specific format.
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    # Use a with statement to ensure proper acquisition and release of resources
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
