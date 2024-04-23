ed10d4fd35c8cd2f1f691c94a3ae71a4920b9733
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-23 16:20:07 -0700

    test

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,30 @@
 #!/usr/bin/node
+
 const request = require('request');
-const pathname = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(pathname, function (error, response, body) {
-  if (!error) {
-    const chars = JSON.parse(body).characters;
-    _recursive_print_chars(chars, 0);
+
+const pathname = `https://swapi-api.alx-tools.com/api/films/`;
+const slug = process.argv[2];
+
+request(pathname + slug, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const res = JSON.parse(body);
+    const chars = res.characters;
+    const recursive = (index) => {
+      if (index >= chars.length) {
+        return;
+      }
+      request(chars[index], (error, response, body) => {
+        if (error) {
+          console.error(error);
+        } else {
+          const char = JSON.parse(body);
+          console.log(char.name);
+          recursive(index + 1);
+        }
+      });
+    };
+    recursive(0);
   }
 });
-
-function _recursive_print_chars(chars, index) {
-  request(chars[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < chars.length) {
-        _recursive_print_chars(chars, index + 1);
-      }
-    }
-  });
-}
