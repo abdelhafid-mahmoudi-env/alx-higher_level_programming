8cd0e1024e1d2ba437905b619d697f02a0034df1
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-23 16:38:00 -0700

    test

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,50 +1,34 @@
 #!/usr/bin/node
+
 const request = require('request');
 
 const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const url = `https://swapi.dev/api/films/${movieId}/`;
+let characters = [];
 
-// Function to fetch movie data
-function fetchMovieData(url) {
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else if (response.statusCode !== 200) {
-        reject(new Error(`Unexpected status code: ${response.statusCode}`));
-      } else {
-        resolve(JSON.parse(body));
-      }
-    });
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  characters = data.characters;
+  getCharacters(0);
+});
+
+const getCharacters = (index) => {
+  if (index === characters.length) {
+    return;
+  }
+
+  request(characters[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    const characterData = JSON.parse(body);
+    console.log(characterData.name);
+    getCharacters(index + 1);
   });
-}
-
-// Function to fetch characters recursively
-async function fetchCharactersRecursively(urls, characters = []) {
-  if (urls.length === 0) {
-    return characters;
-  }
-
-  const [url, ...remainingUrls] = urls;
-  try {
-    const characterData = await fetchMovieData(url);
-    characters.push(characterData.name);
-    return fetchCharactersRecursively(remainingUrls, characters);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-// Print characters in the right order
-async function printCharactersInOrder(movieId) {
-  try {
-    const movieData = await fetchMovieData(apiUrl);
-    const charactersUrls = movieData.characters;
-    const characters = await fetchCharactersRecursively(charactersUrls);
-    characters.forEach(character => console.log(character));
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-printCharactersInOrder(movieId);
+};
